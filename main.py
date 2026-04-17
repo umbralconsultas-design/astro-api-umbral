0408d8432bcc883efae6a3ba4fb79068d058cc26
umbralconsultas-design <umbral.consultas@gmail.com>
2026-04-16 19:34:42 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+from kerykeion import AstrologicalSubject
+
+app = Flask(__name__)
+
+@app.route('/calcular', methods=['POST'])
+def calcular():
+    data = request.json
+
+    subject = AstrologicalSubject(
+        name=data.get("name"),
+        year=int(data.get("year")),
+        month=int(data.get("month")),
+        day=int(data.get("day")),
+        hour=int(data.get("hour")),
+        minute=int(data.get("minute")),
+        city=data.get("city")
+    )
+
+    resultado = {
+        "sun": subject.sun,
+        "moon": subject.moon,
+        "ascendant": subject.ascendant,
+        "planets": subject.planets
+    }
+
+    return jsonify(resultado)
+
+app.run(host='0.0.0.0', port=10000)
