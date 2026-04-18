3b61d1d549c51cb6ac8623c1d9cd1fc407e1955e
umbralconsultas-design <umbral.consultas@gmail.com>
2026-04-18 10:58:38 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,10 @@
-from flask import Flask, request, jsonify
-import requests
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "API UMBRAL funcionando"
-
-@app.route("/carta", methods=["POST"])
-def carta():
-    data = request.json
-
-    url = "https://jsonplaceholder.typicode.com/posts"
-
-    response = requests.post(url, json=data)
-
-    return jsonify({
-        "status": "ok",
-        "data_recibida": data
-    })
+    return "UMBRAL OK"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
