2aa6ac156f26416348e3af6f139f02309862f8d5
umbralconsultas-design <umbral.consultas@gmail.com>
2026-04-18 11:24:53 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "UMBRAL OK"
+
+@app.route("/carta", methods=["POST"])
+def carta():
+    data = request.json
+
+    return jsonify({
+        "mensaje": "Carta recibida",
+        "datos": data
+    })
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
